9bccb8d6e07261eea6e2d388941ad230d57572fa
technoblogical <technoblogical@UbuntuLAMP.(none)>
2013-11-08 23:17:18 -0600

    Who needs a CSS file?

--- README.md
+++ README.md
@@ -4,21 +4,26 @@ by <a href='https://plus.google.com/104536213394512642005?rel=author'>Chris Walk
 
 This is a jQuery gallery that I built for a client to display images on his webpage. (It was a rework of two similar image galleries. It seems that making bullet lists are too difficult.) It works well with WordPress. If the post or page has more than 15 images on it, the jQuery kicks in and builds the gallery. It is required that the images be in a paragraph, but this is the default for Wordpress. The jQuery in the plugin will then find the images and build a bunch of little boxes for the individual images. It also builds one large box to show the first image. As the small boxes are clicked, the images contained within them display in the large box. The titles of the images are transferred to the small boxes.
 
-##How does this thing work?
-The directions to use this plugin are pretty straight forward. This isn't a polished product. I'll admit it isn't great, but it works and that's all that matters. These are the bullet points to remember.
+##How Does This Thing Work?
+The directions to use this plugin are pretty straight forward. This isn't a polished product. I'll admit it isn't great, but it works and that's all that matters. These are the bullet points to remember. I built it to use in WordPress themes, so...
 
+###Directions For WordPress Themes...
+1. Place the dumbGallery directory in your root theme directory.
+2. Link to jQuery.
+3. Link to dumbGallery.js in the dumbGallery directory.
+4. Create a post/page with at least 15 images. (Those images need to be in paragraph, but that's the WordPress default.)
+5. Titles on the images also transfer to the thumbnails in the image gallery.
+
+###Slightly Nerdier Directions...
 1. These images need to use the IMG tag. (What else is there?)
-2. These images need to be in a paragraph.
-3. This will find the images if they are in multiple paragraphs.
-4. Having them in hyperlinks is optional.
-5. There need to be a minimum of 15 images on the page.
-6. Only one gallery per page will be built.
-7. The gallery is placed after the last photo's parent item.
-8. I built it this way, because that's the way WordPress embeds images by default.
-9. Also, the script will find the title of each image and make that the title of the little boxes.
-10. Finally, the script will hide the images. I did this because the little boxes can not have their image stolen by right clicking and pulling up the context menu. (You can still get it from the CSS, though.)
+2. These images need to be in a paragraph. (This will find the images if they are in multiple paragraphs.)
+3. There need to be a minimum of 15 images on the page.
+4. Only one gallery per page will be built. (The gallery is placed after the last photo's parent item.)
+5. Also, the script will find the title of each image and make that the title of the little boxes.
+6. Finally, the script will hide the images. I did this because the little boxes can not have their image stolen by right clicking and pulling up the context menu. (You can still get it from the CSS, though.)
+7. Link your page to jQuery and the dumbGallery.js in the dumbGallery directory.
 
-##License
+####License
 Copyright (c) 2013 Chris Walker
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
--- dumbGallery/dumbGallery.js
+++ dumbGallery/dumbGallery.js
@@ -1,61 +1,67 @@
-$('head').append('<link rel="stylesheet" type="text/css" href="dumbGallery/dumbGallery.css">');
-
-//This is the function that builds the gallery when called.
+					//This is the function that builds the gallery when called.
 function dumbGalleryFunction(){
-
-	//need a string to hold the HTML of the gallery
+							//need a string to hold the HTML of the gallery
 	var dumbGallery ='';
-
-	//Used to test if this is the first photo in the set.
+							//Used to test if this is the first photo in the set.
 	var galleryBigBoxBackground=false;
 	$('p img').each(function(){
-
-		//Gets the image URL
+							//Gets the image URL
 		var imageURL = $(this).attr('src');
-
-		//Gets the Image title (If there is one!)
+							//Gets the Image title (If there is one!)
 		if ($(this).attr('title')){
 			var imageTitle = $(this).attr('title');
 		} else {
 			var imageTitle = '';
 		};
-
-		//If this is still the first image, galleryBigBoxBackground is still false
-		//If it is, then this assigns it the value of the first image URL.
-		// Used later to make the big box appear with the first image. Since it is now a string, it will evaluate as true next time. 
+							//If this is still the first image, galleryBigBoxBackground is still false
+							//If it is, then this assigns it the value of the first image URL.
+							// Used later to make the big box appear with the first image. Since it is now a string, it will evaluate as true next time. 
 		if (!galleryBigBoxBackground) {
 			galleryBigBoxBackground= $(this).attr('src');
-
-			//Adds the big box to beginning of the string
+									//Adds the big box to beginning of the string
 			dumbGallery += '<div class="galleryBigBox" style="background-image:url(\''+galleryBigBoxBackground+'\')"></div>';
 		};
-
-		//This starts adding the little boxes. One for each image.
-		//Makes the background image what it needs to be. Makes the title of the div what it needs to be.
+								//This starts adding the little boxes. One for each image.
+								//Makes the background image what it needs to be. Makes the title of the div what it needs to be.
 		dumbGallery += '<div class="galleryLittleBox" style="background-image:url(\''+imageURL+'\')" title="'+imageTitle+'"></div>';
 	});
-
-	//These boxes are floating. We need to clear that float to preserve CSS that comes after this.
+							//These boxes are floating. We need to clear that float to preserve CSS that comes after this.
 	dumbGallery +='<div style="clear:both;"></div>';
-
-	//insert the gallery after the last image on the page.
+							//insert the gallery after the last image on the page.
 	$('p img').last().parent().parent().after(dumbGallery);
-
-	//hides all images in paragraphs.
+							//hides all images in paragraphs.
 	$('p img').hide();
 };
 
-//If there are more than 15 images in p tags on this page, this calls the function above.
+						//If there are more than 15 images in p tags on this page, this calls the function above.
 if($('p img').length>=15) {
 	dumbGalleryFunction();	
 }
 
-//This executes when you click on the little boxes
+						//This executes when you click on the little boxes
 $('.galleryLittleBox').click(function(){
-
-	//gets the background for the little box...
+							//gets the background for the little box...
 	var imageURL = $(this).css('background-image');
-
-	//And changes the big box background to match.
+							//And changes the big box background to match.
 	$('.galleryBigBox').css('background-image',imageURL);
+});
+
+							//applying the needed CSS
+$('.galleryBigBox').css({
+	'background-position': 'top left',
+	'background-repeat': 'no-repeat',
+	'background-size': '400px',
+	'border': '0px solid black',
+	'float': 'right',
+	'height': '462px',
+	'width': '450px'
+});
+$('.galleryLittleBox').css({
+	'background-repeat':'no-repeat',
+	'background-size':'75px',
+	'border':'0px solid black',
+	'float':'left',
+	'height':'75px',
+	'margin':'1px',
+	'width':'75px'
 });
